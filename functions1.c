94803340989d3ea038d677a7af6126b25f676c7d
SibusisoDM <sdmangwane@gmail.com>
2022-10-20 02:53:26 -0700

    second commit

--- functions1.c
+++ functions1.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
-/****************** PRINT UNSIGNED NUMBER ********/
+/************** PRINT UNSIGNED NUMBER ***************/
 /**
  * print_unsigned - Prints an unsigned number
- * @types: List of arguments
- * @buffer: Buffer array handle print
+ * @types: List a of aurguments
+ * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: get width
  * @precision: Precision specification
@@ -22,7 +22,7 @@ int print_unsigned(va_list types, char buffer[],
 	if (num == 0)
 		buffer[i--] = '0';
 
-	buffer[BUFF_SIZE -1] = '\0';
+	buffer[BUFF_SIZE - 1] = '\0';
 
 	while (num > 0)
 	{
@@ -33,18 +33,17 @@ int print_unsigned(va_list types, char buffer[],
 	i++;
 
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+
 }
 
-/************* PRINT UNSIGNED NUMBER IN OCTAL **************/
+/********************* PRINT UNSIGNED NUMBER IN OCTAL *****************/
 /**
  * print_octal - Prints an unsigned number in octal notation
- * @types: Lists of arguments
+ * @types: List of arguments
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: get width
- * @precision: Precision specification
- * @size: SIze specifier
- * Return: Number of chars printed
+ * @precision: Number of chars printed
  */
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -74,35 +73,18 @@ int print_octal(va_list types, char buffer[],
 	i++;
 
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+
 }
 
-/************ PRINT UNSIGNED NUMBER IN HEXADECIMAL *************/
-/**
- * print_hexadecimal - Prints an unsigned number in hexadecimal notation
- * @types: list arguments
- * @buffer: Buffer array to handle print
- * @flags: Calculates active flags
- * @width: get width
- * @precision: Precision specification
- * @size: Size specification
- * Return: Number of chars printed
- */
-int print_hexadecimal(va_list types, char buffer[],
-	int flags, int width, int precision, int size)
-{
-	return (print_hexa(types, "0123456789abcdef", buffer,
-		flags, 'x', width, precision, size));
-}
-
-/***************** PRINT UNSIGNED IN UPPER HEXADECIMAL *************/
+/************* PRINT UNSIGNED NUMBER IN HEXADECIMAL ************/
 /**
  * print_hexa_upper - Prints an unsigned number in upper hexadecimal notation
- * @types: Lists of arguments
+ * @tpes: List of arguments
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
  * @width: get width
  * @precision: Precision specification
- * @size: size specifier
+ * @size: Size specifier
  * Return: Number of chars printed
  */
 int print_hexa_upper(va_list types, char buffer[],
@@ -112,23 +94,22 @@ int print_hexa_upper(va_list types, char buffer[],
 		flags, 'X', width, precision, size));
 }
 
-/*************** PRINT HEXX NUM IN LOWER OR UPPER ************/
+/****************** PRINT HEXX IN LOWER OR UPPER ****************/
 /**
- * print_hexa - Prints hexadecimal number in lower or upper
- * @types: Lists of arguments
+ * print_hexa - Prints a hexadecimal number in lower or upper
+ * @types: List of argument
  * @map_to: Array of values to map the number to
  * @buffer: Buffer array to handle print
  * @flags: Calculates active flags
+ * @flags_ch: Calculates active flags
  * @width: get width
- * @precision: Precision specifications
- * @size: Size specifier
- * @size: Size specification
+ * @precision: Precision sprecification
  * Return: Number of chars printed
  */
 int print_hexa(va_list types, char map_to[], char buffer[],
 	int flags, char flag_ch, int width, int precision, int size)
 {
-	int i = BUFF_SIZE -2;
+	int i = BUFF_SIZE - 2;
 	unsigned long int num = va_arg(types, unsigned long int);
 	unsigned long int init_num = num;
 
@@ -156,4 +137,5 @@ int print_hexa(va_list types, char map_to[], char buffer[],
 	i++;
 
 	return (write_unsgnd(0, i, buffer, flags, width, precision, size));
+
 }
